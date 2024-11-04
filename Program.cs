9ac122ec904745a01ed91d664925398f2256805f
quinc <quinc@QUINCYPC>
2024-11-04 16:27:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace assignment1_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string myName = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            double age = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your address?");
+            string address = Console.ReadLine();
+            Console.WriteLine("My name is " +myName + ",I am " +age +",and I live on " +address +".");
+        }
+    }
+}
